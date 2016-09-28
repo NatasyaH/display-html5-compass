31ebb029086e4cb433985e6db733136c5e30d120
Christopher Watts <chris@byhook.com>
2016-09-28 14:38:18 -0700

    stubbing in preloader

--- app/App.js
+++ app/App.js
@@ -1,13 +1,25 @@
 'use strict';
 
 var adKit = require ('./hook-ad-kit/AdKit');
+var RSVP = require('rsvp');
 
 var App = function () {
 
   console.log ("hello app");
 
-  adKit.boot().then(function (){ console.log ("App Booted"); })
+  adKit.boot()
+    .then(function (value){ console.log ("App Booted"); return value })
+    .then (preload)
+    .then (function (value) {console.log ("preloaded")});
 
+
+  function preload () {
+
+    var stub = function () {return new RSVP.Promise (function (resolve,reject){ return resolve()} );};
+    //add additional preloaders here
+
+    return RSVP.all ([stub])
+  }
 
 };
 
