de9a98c8a1725ba197ca9a6fecdb0c7c4270f520
Christopher Watts <chris@byhook.com>
2016-10-03 12:50:53 -0700

    loading background images in divs

--- app/hook-ad-kit/AdKit.js
+++ app/hook-ad-kit/AdKit.js
@@ -34,12 +34,12 @@ var AdKit = {
 
     return new RSVP.Promise(function (resolve, reject) {
 
-      function loadComplete(response) {
+      var loadComplete = function (response) {
 
-        console.log ('partial loaded');
+        console.log('partial loaded');
 
         return resolve(response);
-      }
+      };
 
       var xhr = new XMLHttpRequest();
       xhr.open('GET', url);
@@ -59,12 +59,51 @@ var AdKit = {
   },
   subloadPartial: function (container, html) {
 
-    console.log ('subloadPartial');
+    console.log('subloadPartial');
 
+    var loadBackgroundImg = function (target) {
 
-    function loadImgTags(target) {
+      console.log('load background images');
 
-      console.log ('loadImgTags');
+      var divs = target.querySelectorAll('div');
+
+      var retArray = [];
+
+      for (var i = 0; i < divs.length; i++) {
+        var obj = divs[i];
+
+        var imagePath = window.getComputedStyle(obj).getPropertyValue("background-image").match (/http(.*?).(?:jpg|gif|png)/);
+
+        if (imagePath !== null  ) {
+
+          imagePath = imagePath[0];
+          console.log('load background image =', imagePath);
+
+          retArray.push(
+            new RSVP.Promise(function (resolve, reject) {
+
+              var img = new Image();
+              img.onload = function () {
+
+                console.log('single background loaded');
+
+                resolve()
+              }.bind(this);
+              img.src = imagePath;
+
+            })
+          )
+
+        }
+
+      }
+      return RSVP.all(retArray);
+
+    };
+
+    var loadImgTags = function (target) {
+
+      console.log('loadImgTags');
 
       var retArray = [];
 
@@ -80,7 +119,7 @@ var AdKit = {
             var img = new Image();
             img.onload = function () {
 
-              console.log ('single image loaded');
+              console.log('single image loaded');
 
               resolve()
             }.bind(this);
@@ -92,18 +131,23 @@ var AdKit = {
       }
 
       return RSVP.all(retArray);
-    }
+    };
 
     return new RSVP.Promise(function (resolve, reject) {
       util.removeChildren(container)
-        .then(function (){ console.log ('add html'); container.innerHTML = html  })
-        .then(function () {return loadImgTags(container)}) // need to return promise to keep flow going since it is async
+        .then(function () {
+          console.log('add html');
+          container.innerHTML = html
+        })
+        .then(function () {
+          return RSVP.all([
+            loadImgTags(container),
+            loadBackgroundImg(container)
+
+          ])
+
+        }) // need to return promise to keep flow going since it is async
         .then(resolve)
-
-
-
-
-
 
     });
 
@@ -112,26 +156,22 @@ var AdKit = {
 
     return new RSVP.Promise(function (resolve, reject) {
 
-      console.log ('expansion requested')
+      console.log('expansion requested')
 
       // only allow expand if not expanding already
       if (Enabler.getContainerState() !== studio.sdk.ContainerState.EXPANDED && Enabler.getContainerState() !== studio.sdk.ContainerState.EXPANDING) {
 
-
-
         var func = function () {
-          Enabler.removeEventListener (studio.events.StudioEvent.EXPAND_START,func);
+          Enabler.removeEventListener(studio.events.StudioEvent.EXPAND_START, func);
           resolve('EXPANSION START')
         };
 
-        Enabler.addEventListener(studio.events.StudioEvent.EXPAND_START,func );
+        Enabler.addEventListener(studio.events.StudioEvent.EXPAND_START, func);
         Enabler.requestExpand();
-
 
       } else {
         reject('AlreadyExpanded');
       }
-
 
     });
 
@@ -140,23 +180,21 @@ var AdKit = {
 
     return new RSVP.Promise(function (resolve, reject) {
 
-      console.log ('complete expansion requested')
+      console.log('complete expansion requested')
 
       if (Enabler.getContainerState() === studio.sdk.ContainerState.EXPANDING) {
 
         var func = function () {
-          Enabler.removeEventListener (studio.events.StudioEvent.EXPAND_FINISH,func);
+          Enabler.removeEventListener(studio.events.StudioEvent.EXPAND_FINISH, func);
           resolve('EXPANSION COMPLETE')
         };
 
         Enabler.addEventListener(studio.events.StudioEvent.EXPAND_FINISH, func);
         Enabler.finishExpand();
 
-
       } else {
         reject('Expand Not Started so cant be completed');
       }
-
 
     });
 
@@ -170,21 +208,18 @@ var AdKit = {
       // only collapse if expanded
       if (Enabler.getContainerState() == studio.sdk.ContainerState.EXPANDED) {
 
-
         var func = function () {
-          Enabler.removeEventListener (studio.events.StudioEvent.COLLAPSE_START,func);
-          console.log ('!!!!!');
+          Enabler.removeEventListener(studio.events.StudioEvent.COLLAPSE_START, func);
+          console.log('!!!!!');
           resolve('COLLAPSE START')
         };
 
-        Enabler.addEventListener(studio.events.StudioEvent.COLLAPSE_START,func);
+        Enabler.addEventListener(studio.events.StudioEvent.COLLAPSE_START, func);
         Enabler.requestCollapse();
-
 
       } else {
         reject('AlreadyCollapsed');
       }
-
 
     });
 
@@ -194,12 +229,10 @@ var AdKit = {
 
     return new RSVP.Promise(function (resolve, reject) {
 
-
       if (Enabler.getContainerState() === studio.sdk.ContainerState.COLLAPSING) {
 
-
         var func = function () {
-          Enabler.removeEventListener (studio.events.StudioEvent.COLLAPSE_FINISH,func);
+          Enabler.removeEventListener(studio.events.StudioEvent.COLLAPSE_FINISH, func);
 
           resolve('COLLAPSE COMPLETE')
         };
@@ -207,28 +240,20 @@ var AdKit = {
         Enabler.addEventListener(studio.events.StudioEvent.COLLAPSE_FINISH, func);
         Enabler.finishCollapse();
 
-
       } else {
         reject('Collapse not started so cant complete');
       }
-
 
     });
 
   },
 
   exit: function (closure) {
-    return new RSVP.Promise(
+    return new RSVP.Promise(function (resolve, reject) {
+      Enabler.addEventListener(studio.events.StudioEvent.EXIT, resolve);
+      closure.call();
 
-
-
-
-      function (resolve, reject) {
-        Enabler.addEventListener(studio.events.StudioEvent.EXIT, resolve);
-        closure.call();
-
-
-      })
+    })
   },
 
   expanded: function () {
@@ -240,7 +265,6 @@ var AdKit = {
     }
 
     return false;
-
 
   }
 
