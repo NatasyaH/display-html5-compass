2bad0d8ac320c9d6a803dd04e9deea4fcbab0a10
Christopher Watts <chris@byhook.com>
2016-10-17 14:45:44 -0700

    adding zip rename to bundle

--- gulp/tasks/bundle-dc.js
+++ gulp/tasks/bundle-dc.js
@@ -5,7 +5,9 @@
 'use strict';
 var zip = require('gulp-vinyl-zip').zip;
 var gutil = require('gulp-util');
-
+var fs = require("fs");
+var del = require('del');
+var path = require('path');
 /**
  * @param gulp - function
  * @param options - object
@@ -15,16 +17,14 @@ var gutil = require('gulp-util');
  * options.sourcemap : determines if sourcemaps are to be generated
  * @returns {Function}
  */
-module.exports = function (gulp,  options, flags) {
+module.exports = function (gulp, options, flags) {
   return function () {
-
-
-
-    return gulp.src(options.src).on('error',gutil.log)
-
-            .pipe(zip(options.name).on('error',gutil.log))
-
-            .pipe(gulp.dest(options.dist).on('error',gutil.log));
+    var arr = path.resolve(options.dist, '../');
+    arr = arr.split(path.sep);
+    var name = arr[arr.length - 1] + '.zip';
+    return gulp.src(options.src).on('error', gutil.log)
+      .pipe(zip(name).on('error', gutil.log))
+      .pipe(gulp.dest(options.dist).on('error', gutil.log));
   }
 };
 
