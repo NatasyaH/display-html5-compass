45443300ba9fc8da18120eac12a0c7af5e9a10a5
Christopher Watts <chris@byhook.com>
2016-10-17 15:40:22 -0700

    working on manifest file

---
+++ static/html/manifest.js
@@ -0,0 +1,16 @@
+FT.manifest({
+  "filename": "index.html",
+  "width": 970,
+  "height": 66,
+  "clickTagCount": 2,
+  "hideBrowsers": ["ie9"],
+  "expand":{
+    "width": 970,
+    "height": 418,
+    "indentAcross": 0,
+    "indentDown": 0
+  },
+  "richLoads": [
+    {"name": "rich", "src": "rich"},
+    ]
+});
