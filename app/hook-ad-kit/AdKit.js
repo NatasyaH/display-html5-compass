804d263125dcc374e877156360abcdfd219f16a5
Christopher Watts <chris@byhook.com>
2016-11-03 18:32:11 -0700

    working on new structure

---
+++ app/hook-ad-kit/AdKit.js
@@ -0,0 +1,24 @@
+'use strict';
+var AdKit = function (kit) {
+  var ret = {};
+  var mapToObject = function (source, target) {
+    var props = Object.getOwnPropertyNames(source);
+    for (var i = 0; i < props.length; i++) {
+      target[props[i]] = source[props[i]];
+    }
+  };
+
+  mapToObject(require('./loading')(), ret);
+
+  if (kit === AdKit.DC) {
+    mapToObject(require('./state/DC')(), ret);
+    mapToObject(require('./loading/url/DC')(), ret);
+
+  }
+  if (kit === AdKit.FT) {
+  }
+  return ret;
+};
+AdKit.DC = "DC";
+AdKit.FT = "FlashTalking";
+module.exports = AdKit;
