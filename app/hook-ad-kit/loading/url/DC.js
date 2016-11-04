defd2cae9dbabb5b4ae51fb35edc78ca794e68ad
Christopher Watts <chris@byhook.com>
2016-11-04 15:15:03 -0700

    clean up

--- app/hook-ad-kit/loading/url/DC.js
+++ app/hook-ad-kit/loading/url/DC.js
@@ -1,11 +1,6 @@
 'use strict';
 module.exports = function () {
   var getRichBase = function (baseURL) {
-    var arr = baseURL.split('/');
-    // named folder of base
-    var baseFolder = arr[arr.length - 1];
-    var richFolder = baseFolder + '-rich/';
-    // dontt need seperate base URLs in DC
     return baseURL.slice(0);
   };
   return {
