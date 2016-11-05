483ef7defa6994e5037dbb933cb684d6706d589e
Christopher Watts <chris@byhook.com>
2016-11-04 17:36:08 -0700

    removing debug testing thing

--- app/hook-ad-kit/loading/index.js
+++ app/hook-ad-kit/loading/index.js
@@ -31,7 +31,7 @@ module.exports = function () {
         return resolve(response);
       };
       var xhr = new XMLHttpRequest();
-      xhr.open('GET', urlss);
+      xhr.open('GET', url);
       xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
           loadComplete(xhr.responseText);
