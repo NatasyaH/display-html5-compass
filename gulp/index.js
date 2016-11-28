f24d439ec0a9a6a3f58a7b333fea6746e6449d30
Christopher Watts <chris@byhook.com>
2016-11-28 10:19:01 -0800

    adding ability to set key

--- gulp/index.js
+++ gulp/index.js
@@ -3,6 +3,19 @@
 var gulp = require('gulp');
 var config = require('./config');
 var bs = require('browser-sync').create();
+var argv = require('minimist')(process.argv.slice(2));
+var nconf = require('nconf');
+var path = require('path');
+var os = require('os');
+var fs = require('fs');
+console.log('vars');
+console.log(argv);
+var opts = {
+  tinypngkey: null
+};
+nconf.env()
+  .file({file: path.join(path.resolve(os.homedir()), 'display-standard-previewer-config.json')});
+opts.tinypngkey = nconf.get('tinypngkey');
 // two more states to minify code and create sourcemaps. The default is for local development.
 gulp.task('dev', function (done) {
   config.flags.minify = false;
@@ -13,11 +26,9 @@ gulp.task('dev', function (done) {
 gulp.task('prod', function (done) {
 
 //test
-
   config.flags.minify = true;
   config.flags.sourcemap = false;
   config.flags.type = 'prod';
-
   done();
 });
 // define stackable tasks
@@ -27,16 +38,14 @@ gulp.task('ft-manifest', require('./tasks/ft-manifest')(gulp, bs, config.ft_mani
 gulp.task('sass', require('./tasks/lib-sass')(gulp, bs, config.sass, config.flags));
 gulp.task('images', require('./tasks/images')(gulp, bs, config.images));
 gulp.task('videos', require('./tasks/videos')(gulp, bs, config.videos));
-
 gulp.task('sprite-collapsed-foreground', require('./tasks/sprite-images')(gulp, bs, config.sprite.collapsed_foreground, config.flags));
 gulp.task('sprite-collapsed-background', require('./tasks/sprite-images')(gulp, bs, config.sprite.collapsed_background, config.flags));
 gulp.task('sprite-expanded-foreground', require('./tasks/sprite-images')(gulp, bs, config.sprite.expanded_foreground, config.flags));
 gulp.task('sprite-expanded-background', require('./tasks/sprite-images')(gulp, bs, config.sprite.expanded_background, config.flags));
 gulp.task('sprite-auto-expanded-foreground', require('./tasks/sprite-images')(gulp, bs, config.sprite.auto_expanded_foreground, config.flags));
 gulp.task('sprite-auto-expanded-background', require('./tasks/sprite-images')(gulp, bs, config.sprite.auto_expanded_background, config.flags));
-gulp.task ('sprite-all', gulp.parallel ('sprite-collapsed-foreground','sprite-collapsed-background','sprite-expanded-foreground','sprite-expanded-background','sprite-auto-expanded-foreground','sprite-auto-expanded-background'));
+gulp.task('sprite-all', gulp.parallel('sprite-collapsed-foreground', 'sprite-collapsed-background', 'sprite-expanded-foreground', 'sprite-expanded-background', 'sprite-auto-expanded-foreground', 'sprite-auto-expanded-background'));
 //gulp.task('sprite-convert', require('./tasks/sprite-convert')(gulp, config.sprite.optimize, config.flags));
-
 gulp.task('scripts-app', require('./tasks/scripts-app')(gulp, bs, config.scripts, config.flags));
 // dev build specific tasks
 gulp.task('scripts-vendor-dev', require('./tasks/scripts-vendor-dev')(gulp, bs, config.vendor, config.flags));
@@ -46,11 +55,17 @@ gulp.task('optimize-css', require('./tasks/optimize-css')(gulp, config.optimize,
 gulp.task('optimize-js', require('./tasks/optimize-js')(gulp, config.optimize, config.flags));
 gulp.task('optimize-html', require('./tasks/optimize-html')(gulp, config.optimize, config.flags));
 gulp.task('rename-standard', require('./tasks/rename-standard')(gulp, bs, config.rename_backup, config.flags));
-
-
 gulp.task('bundle-dc', require('./tasks/bundle-dc')(gulp, config.bundle.dc, config.flags));
 gulp.task('bundle-ft', require('./tasks/bundle-ft')(gulp, config.bundle.ft, config.flags));
-
+gulp.task('set-key', function (done) {
+  nconf.set('tinypngkey', argv['key']);
+  nconf.save(function (err) {
+    fs.readFile(path.join(path.resolve(os.homedir()), 'display-standard-previewer-config.json'), function (err, data) {
+      console.dir(JSON.parse(data.toString()));
+      done()
+    });
+  });
+})
 // define watch actions
 gulp.task('watch', function (done) {
   bs.init({
@@ -72,7 +87,6 @@ gulp.task('watch', function (done) {
   gulp.watch(config.scripts.app.src, gulp.series('scripts-app'));
   gulp.watch(config.html.src, gulp.series('html'));
   gulp.watch(config.sass.watch_src, gulp.series('sass')); // only watch files not associated with spritesheets
-
   // generate new sass partials for spritesheets when images edited
   gulp.watch(config.sprite.collapsed_foreground.src, gulp.series('sprite-collapsed-foreground'));
   gulp.watch(config.sprite.collapsed_background.src, gulp.series('sprite-collapsed-background'));
@@ -80,7 +94,6 @@ gulp.task('watch', function (done) {
   gulp.watch(config.sprite.expanded_background.src, gulp.series('sprite-expanded-background'));
   gulp.watch(config.sprite.auto_expanded_foreground.src, gulp.series('sprite-auto-expanded-foreground'));
   gulp.watch(config.sprite.auto_expanded_background.src, gulp.series('sprite-auto-expanded-background'));
-
   if (config.flags.type === "dev") {
     gulp.watch(config.vendor.src, gulp.series('scripts-vendor-dev'));
   }
@@ -89,8 +102,8 @@ gulp.task('watch', function (done) {
   }
   done();
 });
-gulp.task('build-dev', gulp.series('dev', 'clean', gulp.parallel('html','ft-manifest', 'scripts-vendor-dev', 'scripts-app', 'images', 'videos',gulp.series('sprite-all', 'sass','rename-standard' ))));
-gulp.task('build-prod', gulp.series('prod', 'clean', gulp.parallel('html','ft-manifest', 'scripts-vendor', 'scripts-app', 'images', 'videos', gulp.series('sprite-all','sass','rename-standard' ))));
+gulp.task('build-dev', gulp.series('dev', 'clean', gulp.parallel('html', 'ft-manifest', 'scripts-vendor-dev', 'scripts-app', 'images', 'videos', gulp.series('sprite-all', 'sass', 'rename-standard'))));
+gulp.task('build-prod', gulp.series('prod', 'clean', gulp.parallel('html', 'ft-manifest', 'scripts-vendor', 'scripts-app', 'images', 'videos', gulp.series('sprite-all', 'sass', 'rename-standard'))));
 gulp.task('build-prod-optimize', gulp.series('build-prod', gulp.parallel('optimize-css', 'optimize-js', 'optimize-html')));
 gulp.task('watch-dev', gulp.series('dev', 'build-dev', 'watch'));
 gulp.task('watch-prod', gulp.series('prod', 'build-prod', 'watch'));
